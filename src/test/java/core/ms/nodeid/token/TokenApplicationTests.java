5dadcdd82542af4fe6833f1848be92be7b5ea89a
eng-ivan <eng.ivancarlos85@gmail.com>
2023-09-02 17:02:04 -0300

    change Java/Maven version & add assert test

--- src/test/java/core/ms/nodeid/token/TokenApplicationTests.java
+++ src/test/java/core/ms/nodeid/token/TokenApplicationTests.java
@@ -1,13 +1,29 @@
 package core.ms.nodeid.token;
 
+import core.ms.nodeid.token.model.Token;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TokenApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void test() {
+			UUID id = UUID.randomUUID();
+			String token = "meuToken";
+			LocalDateTime timestamp = LocalDateTime.now();
 
+			Token object = new Token(id, token, timestamp);
+
+			assertNotNull(object);
+			assertEquals(id, object.getId());
+			assertEquals(token, object.getToken());
+			assertEquals(timestamp, object.getTimestamp());
+	}
 }
